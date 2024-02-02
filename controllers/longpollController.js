a7e60aab9b3d118850ec9476c791125c8e3cfa49
IMRANDIL <aliimranadil2@gmail.com>
2024-02-02 19:22:18 +0530

    To address the potential concurrency issues when modifying the clients array, you can use a synchronization mechanism like a lock.

--- controllers/longpollController.js
+++ controllers/longpollController.js
@@ -1,5 +1,6 @@
 let data = "Initial data";
 let clients = [];
+let isUpdating = false; // Lock variable
 const updateInterval = 5000;
 const cleanupInterval = 30000;
 
@@ -9,13 +10,24 @@ exports.longPollController = async (req, res, next) => {
       id: Math.random().toString(36).slice(2),
       res,
     };
-    clients.push(newClient);
 
-    setTimeout(() => {
-      data = `Updated data at ${new Date()}`;
-      res.json({ data });
-      clients = clients.filter((client) => client !== newClient);
-    }, updateInterval);
+    // Acquire the lock
+    if (!isUpdating) {
+      isUpdating = true;
+
+      clients.push(newClient);
+
+      setTimeout(() => {
+        data = `Updated data at ${new Date()}`;
+        res.json({ data });
+        clients = clients.filter((client) => client !== newClient);
+
+        // Release the lock
+        isUpdating = false;
+      }, updateInterval);
+    } else {
+      res.status(503).json({ error: "Service Unavailable" });
+    }
   } catch (error) {
     console.error("Error in longPollController:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -23,10 +35,18 @@ exports.longPollController = async (req, res, next) => {
 };
 
 setInterval(() => {
-  clients.forEach((client) => {
-    if (!client.res.finished) {
-      client.res.json({ data });
-      clients = clients.filter((c) => c !== client);
-    }
-  });
+  // Acquire the lock
+  if (!isUpdating) {
+    isUpdating = true;
+
+    clients.forEach((client) => {
+      if (!client.res.finished) {
+        client.res.json({ data });
+        clients = clients.filter((c) => c !== client);
+      }
+    });
+
+    // Release the lock
+    isUpdating = false;
+  }
 }, cleanupInterval);
